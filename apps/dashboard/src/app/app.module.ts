cb609fe51bd088e2a1665e447439389f4048c9ef
XdaTk <fq19930317@gmail.com>
2018-12-22 10:02:23 +0800

    引入BootStrap

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
-
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +11,7 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     BrowserAnimationsModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   bootstrap: [AppComponent]
 })
---
+++ libs/styles/src/lib/styles.module.spec.ts
@@ -0,0 +1,7 @@
+import { StylesModule } from './styles.module';
+
+describe('StylesModule', () => {
+  it('should create', () => {
+    expect(StylesModule).toBeDefined();
+  });
+});
